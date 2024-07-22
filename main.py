73d738eb62227d31815ec329020b26aa794efae8
Soaham Sur <145566356+SoahamSur@users.noreply.github.com>
2024-07-22 10:05:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pyperclip
 import time
 import google.generativeai as genai
 import re
-
+#Change co-ordinates according to your system using getCursor.py
 # Configure the API key for Google Generative AI
-gemini_key = "AIzaSyDl-Ksq6sPBdzMu-60qHDfdJJtX_9cd63c"
+gemini_key = "Your_API_Key"
 genai.configure(api_key=gemini_key)
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
@@ -21,8 +21,8 @@ def extract_senders(chat_log):
     # Find all matches in the chat log
     senders = re.findall(pattern, chat_log)
     
-    # Filter out "Soaham Sur" from the list of sender names
-    filtered_senders = [sender for sender in senders if sender != "Soaham Sur"]
+    # Filter out "YOUR NAME" from the list of sender names
+    filtered_senders = [sender for sender in senders if sender != "YOUR NAME"]
     
     return filtered_senders
 
@@ -44,20 +44,16 @@ while True:
 
     sender = extract_senders(text)
     print(sender)
-    qualtities=[f"Soaham is 19 years old & his height is 6ft4inch",
-                f"Soaham is currently pursuing B.Tech in ELectronics & Communication Enginneering from IIIT Kalyani",
-                f"Soaham's hobbies are photography,playing guitar,listening to music,watching web-series & movies",
-                f"Soaham's nature is friendly and he talks to everyone in a friendly & respectful tone",
-                f"Soaham's mother tongue is Bengali.He also speaks English and hindi."]
-    if is_last_message_from_sender(text, "Soaham Sur"):
+    qualtities=[f"YOUR QUALITIES HERE"]
+    if is_last_message_from_sender(text, "YOUR NAME"):
         prompt2 = (
-            f"You are a human named Soaham. you give normal and human-like answers. "
+            f"You are a human named YOUR NAME. you give normal and human-like answers. "
             f"Your responses are natural, empathetic, and relatable, just like a human being would converse. "
             f"Additionally, ensure that you respond in the same language that the sender uses for their messages. "
             f"Adapt your tone and style to suit casual, friendly interactions, and strive to make your replies engaging and thoughtful. "
-            f"Soaham has the following qualities:{qualtities}"
-            f"Don't generate any message beginning with date ,time and sender's name.Soaham doesn't reply like this. "
-            f"The replies should be short in 1 line. Soaham talks in English mixed with Bengali.He also sometimes speaks Hindi as well depending on the language of the message received. Here's the message: {text}"
+            f"YOUR NAME has the following qualities:{qualtities}"
+            f"Don't generate any message beginning with date ,time and sender's name.YOUR NAME doesn't reply like this. "
+            f"The replies should be short in 1 line. YOUR LANGUAGES.He also sometimes speaks LANGUAGE as well depending on the language of the message received. Here's the message: {text}"
         )
         response = model.generate_content(prompt2)
         response_text = response.candidates[0].content.parts[0].text
